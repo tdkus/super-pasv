7f83442c6238fad00191d257e7efc36770e88170
Oleg Ushakov <ushakov.oa@gmail.com>
2022-04-01 13:03:54 -0400

    Mocha hooks (#5)
    * add first tests
    add mocha config
    
    * add auth tests
    
    * add enviroment variables support
    
    * add before hooks for auth spec
    add root before hook

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,6 @@
 module.exports = {
     require: '@babel/register',
     spec: 'specs/**/*.spec.js',
-    ignore: 'specs/example.spec.js'
+    ignore: 'specs/example.spec.js',
+    file: 'config/setup.js'
 }
